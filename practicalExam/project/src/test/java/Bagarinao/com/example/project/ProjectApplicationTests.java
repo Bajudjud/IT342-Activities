905d9b805bb106957843171abeb83ed99f0bcf8d
Jude Anthony L. Bagarinao <134255519+Bajudjud@users.noreply.github.com>
2025-03-07 00:32:57 +0800

    practicalExam

---
+++ practicalExam/project/src/test/java/Bagarinao/com/example/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Bagarinao.com.example.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
